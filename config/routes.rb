d02b5751b3dbc0df1d5dcde40d1b31a5fb52f870
Joe Jamsky <cba1067950@yahoo.com>
2019-06-09 22:03:42 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/hello_world', to: 'static#hello_world'
+  # /hello_world is in the controller
+  # static is a folder
+  # # indicates an erb file
+  # #hello_world is the html.erb file
 end
